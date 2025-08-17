4cbfa562a22b13d57f62dd9cac75524de8a817d0
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-17 03:50:27 +0300

    fixed an error

--- photogallery_project/urls.py
+++ photogallery_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel
     path('', include('photogallery_project_app.urls')),  # Include our app's routes
+    path('accounts/', include('django.contrib.auth.urls')),  # ✅ Built-in auth
 ]
--- photogallery_project_app/urls.py
+++ photogallery_project_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -15,6 +16,6 @@ urlpatterns = [
     # Auth
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('profile/<str:username>/', views.profile, name='profile'),
 ]
