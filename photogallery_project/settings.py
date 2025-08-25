bc82c0c7904295b8db696f0b5a38dbdd7e3f359b
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-25 11:33:53 +0300

    1

--- photogallery_project/settings.py
+++ photogallery_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-vrn4gf-b@gl*w!pvg5@9x*yjxkz)he56-6d$ql^qkv@)&!-!(#')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
